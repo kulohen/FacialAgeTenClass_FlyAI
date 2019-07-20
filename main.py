bf0e45228231d3eaf14dfec9f4ac8170e3c02537
kulohen <291255700@qq.com>
2019-07-21 01:24:50 +0800

    update2

--- main.py
+++ main.py
@@ -29,18 +29,18 @@ flyai库中的提供的数据处理方法
 dataset = Dataset(epochs=args.EPOCHS, batch=args.BATCH)
 model = Model(dataset)
 '''
-dataset获取train & test数据的总迭代次数
+dataset.get_train_length() : 5866
+dataset.get_all_validation_data(): 1956
 
 '''
 x_train, y_train , x_val, y_val =dataset.get_all_processor_data()
-
 print('dataset.get_train_length() :',dataset.get_train_length())
 print('dataset.get_all_validation_data():',dataset.get_validation_length())
 '''
 实现自己的网络机构
 '''
 num_classes = 10
-sqeue =ResNet50( weights=None, input_shape=(224, 224, 3), classes= num_classes, include_top=True)
+sqeue =ResNet50( weights=None, input_shape=(200, 200, 3), classes= num_classes, include_top=True)
 
 sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
@@ -52,13 +52,15 @@ sqeue.compile(loss='categorical_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-model.check( MODEL_PATH)
 # 模型保存的路径
+model.check( MODEL_PATH)
 MODEL_PATH_FILE = os.path.join(MODEL_PATH, KERAS_MODEL_NAME)
+
+# callbacks回调函数的定义
 savebestonly = ModelCheckpoint( filepath =MODEL_PATH_FILE, monitor='val_loss', mode='auto', save_best_only=True, verbose=1)
 early_stopping = EarlyStopping(monitor='val_loss', patience=20 ,verbose=1)
 xuexilv = ReduceLROnPlateau(monitor='loss',patience=20, verbose=1)
-best_score = 0
+
 
 x_train_and_x_val = np.concatenate((x_train, x_val),axis=0)
 y_train_and_y_val= np.concatenate((y_train , y_val),axis=0)
@@ -70,21 +72,22 @@ datagen= ImageDataGenerator(
     height_shift_range=0.02,
     shear_range=0.02,
     horizontal_flip=True,
-    vertical_flip=True
-
+    vertical_flip=True,
+    validation_split=0.25
 )
 # datagen.fit(x_train_and_x_val)
-data_iter = datagen.flow(x_train_and_x_val, y_train_and_y_val, batch_size=args.BATCH , save_to_dir = None)
+data_iter_train = datagen.flow(x_train_and_x_val, y_train_and_y_val, batch_size=args.BATCH , save_to_dir = None, subset='training')
+data_iter_validation = datagen.flow(x_train_and_x_val, y_train_and_y_val, batch_size=args.BATCH , save_to_dir = None, subset='validation')
 # 验证集可以也写成imagedatagenerator
 
 print('x_train_and_x_val.shape :', x_train_and_x_val.shape)
 
 
 history = sqeue.fit_generator(
-    data_iter,
-    steps_per_epoch=75,
-    validation_data=(x_val , y_val),
-    # validation_steps=5,
+    generator= data_iter_train,
+    steps_per_epoch=250,
+    validation_data=data_iter_validation,
+    validation_steps=1,
     # callbacks = [early_stopping,xuexilv],
     callbacks = [ savebestonly, xuexilv],
     epochs =args.EPOCHS,
