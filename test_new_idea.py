d9421f6abd1ee38465be119236069a72d59630ab
kulohen <291255700@qq.com>
2019-07-27 11:43:54 +0800

    np.random.choice

--- test_new_idea.py
+++ test_new_idea.py
@@ -2,6 +2,7 @@
 import argparse
 from flyai.dataset import Dataset
 from model import Model
+import numpy as np
 from flyai.utils.yaml_helper import Yaml
 import os
 from path import MODEL_PATH
@@ -32,6 +33,7 @@ print('dataset.get_all_validation_data():',dataset.get_validation_length())
 这里的y_train已经是one-hot形式，没法统计了
 或者针对one-hot来统计
 '''
+print('x_train:',x_train.shape)
 print('y_train.shape:',y_train.shape)
 print('y_train.argmax(1):', y_train[5].argmax())
 
@@ -41,4 +43,20 @@ print('y_val.sum():',y_val.sum(axis=0))
 # print('y_train.count(1):',y_train[0:100][0].count(1))
 
 # yaml = Yaml(path=os.path.join(MODEL_PATH, "app.yaml")).processor()
-model.predict_all(dataset.evaluate_data_no_processor())
+# model.predict_all(dataset.evaluate_data_no_processor())
+'''
+这里测试随机取样
+'''
+array1 =np.array([11,22,33,44,55,66,77,88,99])
+array2 =np.array([111,222,333,444,555,666,777,888,999])
+print('array1.shape[0] :',array1.shape)
+batch_size=5
+slice=np.random.choice(a=array1.shape[0], size=batch_size,replace= False)
+print(array1[slice])
+print(array2[slice])
+
+random_slice = np.random.choice(a=y_train.shape[0], size=2,replace= False)
+print('y_train.shape[0]',y_train.shape[0])
+print('random_slice',random_slice)
+# print(x_train[random_slice])
+# print(y_train[random_slice])
