5915afbc4d2ead0e5f2c22602134cacd1409998b
kulohen <291255700@qq.com>
2019-07-28 23:35:47 +0800

    wangyi Util: csv control

--- WangyiUtilOnFlyai.py
+++ WangyiUtilOnFlyai.py
@@ -14,7 +14,7 @@ import sys
 from flyai.dataset import Dataset
 from flyai.source.base import DATA_PATH
 from flyai.source.source import Source
-
+import pandas as pd
 
 # TODO 构建自己的Util
 
@@ -52,6 +52,29 @@ def readCustomCsv(train_csv_url, test_csv_url):
 
     return source_csv
 
+def ExtendDataFrameToSize(dataframe,size):
+    '''
+
+    :param dataframe: 要被扩展的csv
+    :param size: 扩展到指定size的大小。
+    :return: 比如原csv是12行，size输入25，那么csv至少扩展到25以上，即是12 *2 *2 =48，返回48的大小。
+    '''
+    if dataframe.shape[0] > size :
+        return dataframe
+    else:
+        dataframe = pd.concat([dataframe,dataframe.copy(deep=False)])
+        return ExtendDataFrameToSize(dataframe, size)
+
+def ExtendCsvToSize(source_csv , label='label', size=-1):
+    df5 = pd.DataFrame()
+    for i in range(10):
+        if source_csv[source_csv[label] == i].empty :
+            break
+        tmp = ExtendDataFrameToSize( source_csv[source_csv[label] == i], size)
+        df5 = pd.concat([df5, tmp])
+    # print(df5)
+    # df5.to_csv(os.path.join(DATA_PATH, 'wangyi-1.csv'), index=False)
+    return df5
 
 
 if __name__=='__main__':
@@ -64,4 +87,26 @@ if __name__=='__main__':
     print('dataset2.get_train_length()', dataset2.get_train_length())
     print('dataset2.get_validation_length()', dataset2.get_validation_length())
     xx_train , yy_train= dataset2.next_train_batch()
-    print('yy_train',yy_train)
+    # print('yy_train',yy_train)
+    # save csv
+    df = pd.DataFrame(data=readCustomCsv("test_custom.csv", "test_custom.csv").data)
+    # df.to_csv( os.path.join(DATA_PATH, 'wangyi-1.csv'), index=False)
+    print(df[df['label']==3])
+    # print(df.isin([4, 6]))
+    df2=df.copy(deep=False)
+    df3 = pd.concat([df,df2])
+    print('df3.size',df3.shape)
+    print(df3.shape[0])
+
+    df4 =ExtendDataFrameToSize(df,355)
+    print('df4.shape',df4.shape)
+    # df4.to_csv(os.path.join(DATA_PATH, 'wangyi-1.csv'), index=False)
+    print(df[df['label'] == 7])
+
+    df6 =ExtendCsvToSize(df , label='label' ,size=30)
+    print('df6.shape',df6.shape)
+
+    df7 = pd.DataFrame(data=readCustomCsv("dev.csv", "dev.csv").data)
+    df7 =ExtendCsvToSize(df7 ,size=36)
+    print('df7.shape', df7.shape)
+    df7.to_csv(os.path.join(DATA_PATH, 'wangyi-2.csv'), index=False)
